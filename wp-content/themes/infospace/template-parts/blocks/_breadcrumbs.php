eb17cf0a8a4d87958cb99f1a9327d1aa54fc4aa8
Barry Jarvis <>
2025-12-18 20:17:11 +0000

    news page styling

--- wp-content/themes/infospace/template-parts/blocks/_breadcrumbs.php
+++ wp-content/themes/infospace/template-parts/blocks/_breadcrumbs.php
@@ -1,9 +1,19 @@
 <?php $postype = get_post_type(); ?>
+<?php $current_module_id_global = isset($_SESSION['current_module_id']) ? $_SESSION['current_module_id'] : '';
+$moduleMeta = get_current_module_meta($current_module_id_global);
+
+$moduleCrummbs = '';
+if (isset($moduleMeta['attached_resources']) && !empty($moduleMeta['attached_resources'])) {
+    $moduleId = $moduleMeta['attached_resources'];
+    $moduleName = get_the_title($moduleId);
+    $moduleLink = get_the_permalink($moduleId);
+    $moduleCrummbs = '<a href="' . esc_url($moduleLink) . '">' . esc_html($moduleName) . '</a> &gt; ';
+}
+?>
+
 <div class="yoast-breadcrumbs">
     <?php if ($postype == 'post') { ?>
-        <span><a href="/news/">News</a> &gt; <span class="breadcrumb_last" aria-current="page"><?php the_title(); ?></span></span>
-    <?php } else if ($postype == 'vacancy') { ?>
-        <span><a href="<?php echo get_the_permalink(685); ?>"><?php echo get_the_title(685); ?></a> &gt; <span class="breadcrumb_last" aria-current="page"><?php the_title(); ?></span></span>
+        <span><a href="/">Home</a> &gt; <?php echo $moduleCrummbs; ?><a href="/news/">News</a> &gt; <span class="breadcrumb_last" aria-current="page"><?php the_title(); ?></span></span>
     <?php } else { ?>
         <?php echo do_shortcode('[wpseo_breadcrumb]'); ?>
     <?php } ?>
--- wp-content/themes/infospace/template-parts/content-post.php
+++ wp-content/themes/infospace/template-parts/content-post.php
@@ -12,26 +12,36 @@ $post_id = get_the_ID();
 
 $current_module_id_global = isset($_SESSION['current_module_id']) ? $_SESSION['current_module_id'] : '';
 $moduleMeta = get_current_module_meta($current_module_id_global);
-var_dump($moduleMeta['attached_resources']);
+
 $moduleColour = isset($moduleMeta['module_color']) ? $moduleMeta['module_color'] : '';
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php get_template_part('template-parts/blocks/_breadcrumbs') ?>
 	<header class="resource-module__header full-width" style="background-color: <?php echo esc_html($moduleColour); ?>;">
-		<div class="resource-module__header-inner"  >
+		<div class="resource-module__header-inner">
 			<div class="resource-module__header-content">
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 			</div>
 		</div>
-		
+
 	</header>
 	<div class="entry-content" id="contentskip">
-		<?php echo '<div class="resource-module__news full-width">'; ?>
-		<?php echo '<div class="resource-module__news-container">'; ?>
-		<?php the_content(); ?>
-		<?php echo '<div>'; ?>
+		
+		<?php echo '<div class="resource-page__grid">'; ?>
+			<?php echo '<div class="resource-page__col1">'; ?>
+			<?php the_content(); ?>
+			<?php echo '</div>'; ?>
+			<?php echo '<div class="resource-page__col2">'; ?>
+			<div class="resource-page__box">
+				<p><strong>Published on: <?php echo get_the_date('j F Y'); ?></strong></p>
+				<ul class="child-resources">
+					<li class="side-link"><a href="/news/">Back to news</a></li>
+				</ul>
+			</div>
+			<?php echo '</div>'; ?>
 		<?php echo '</div>'; ?>
+	
 		<?php log_user_interaction(get_permalink(), $post_id, 8, 'Viewed news', get_the_title()); ?>
 		<?php edit_post_link(__('(Edit)', $namespace), '<span class="edit-link">', '</span>'); ?>
 	</div>
