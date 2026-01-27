6a9326ab21f0dc7b82cfc30296ebe9770b834bd7
Barry Jarvis <>
2026-01-27 14:43:08 +0000

    Changed broken link on stats

--- wp-content/themes/infospace/library/stats/stats-pages-list.php
+++ wp-content/themes/infospace/library/stats/stats-pages-list.php
@@ -19,6 +19,6 @@ function reporting_page_callback() {
     echo '<p><a href="admin.php?page=document-report">View document downloads</a></p>';
     echo '<p><a href="admin.php?page=page-views-report">View page views report</a></p>';
     echo '<p><a href="admin.php?page=newsletters-report">View newsletters report</a></p>';
-    echo '<p><a href="admin.php?page=logins-report">View logins report</a></p>';
+    echo '<p><a href="admin.php?page=login-report">View logins report</a></p>';
     echo '</div>';
 }
