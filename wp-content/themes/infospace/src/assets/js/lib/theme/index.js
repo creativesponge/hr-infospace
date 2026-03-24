603ec9aae3d748bddb0aa0d2b097bed0d85047aa
Barry Jarvis <>
2026-03-24 09:37:38 +0000

    removed unused js

--- wp-content/themes/infospace/src/assets/js/lib/theme/index.js
+++ wp-content/themes/infospace/src/assets/js/lib/theme/index.js
@@ -15,7 +15,7 @@ import modules_list from "./blocks/modules-list/index.js";
 import icon_list from "./blocks/icon-list/index.js";
 import benefit_carousel from "./blocks/benefit-carousel/index.js";
 import contact_form from "./blocks/contact-form/index.js";
-import welcome_back from "./blocks/welcome-back/index.js";
+
 
 
 var sitespecific = {
@@ -61,7 +61,6 @@ var sitespecific = {
     icon_list.init();
     benefit_carousel.init();
     //contact_form.init();
-    welcome_back.init();
     
     // Ready to rock!
     document.body.classList.add("ready");
